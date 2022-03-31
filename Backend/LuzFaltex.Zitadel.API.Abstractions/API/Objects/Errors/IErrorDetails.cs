4e8a2d84244e292e8ccb0a36773443829e7d0cd3
Foxtrek_64 <foxtrek64@users.noreply.github.com>
2022-03-31 10:12:18 -0500

    Add Error Models

---
+++ Backend/LuzFaltex.Zitadel.API.Abstractions/API/Objects/Errors/IErrorDetails.cs
@@ -0,0 +1,45 @@
+﻿//
+//  IErrorDetails.cs
+//
+//  Author:
+//       LuzFaltex Contributors
+//
+//  Copyright (c) 2022 LuzFaltex
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using JetBrains.Annotations;
+
+namespace LuzFaltex.Zitadel.API.Abstractions.API.Objects.Errors
+{
+    // TODO: Replace with OneOf and concrete types rather than a ProtobufAny definition.
+
+    /// <summary>
+    /// Represents information about a single error.
+    /// </summary>
+    [PublicAPI]
+    public interface IErrorDetails
+    {
+        /// <summary>
+        /// Gets a url to the schema definition of the type.
+        /// </summary>
+        string TypeUrl { get; }
+
+        /// <summary>
+        /// Gets the value contained by the type.
+        /// </summary>
+        string Value { get; }
+    }
+}
